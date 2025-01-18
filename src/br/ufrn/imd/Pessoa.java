43e0eb0e080905a91772a7b3b1180ec9877f8ab6
cstavictor <cstavictor@gmail.com>
2025-01-18 01:55:41 -0300

    Alteracoes na classe Pessoa, Request e LeituraArquivos

--- src/br/ufrn/imd/Pessoa.java
+++ src/br/ufrn/imd/Pessoa.java
@@ -1,18 +1,77 @@
 package br.ufrn.imd;
 
-//classe de PESSOA para cadastrar e peremtir repetição de nome mas nao de cpf;
+//classe de PESSOA para cadastrar e permitir repetição de nome mas nao de cpf;
+//Atributos de Pessoa
 public class Pessoa {
-    private String nome;
+    private String primeiroNome;
+    private String segundoNome;
+    private String terceiroNome;
     private int idade;
     private String cpf;
+    private String genero;
+    private String profissao;
+    private String data_de_chegada; //adicionar tratamento da data_de_chegada
 
-    public Pessoa(String nome, int idade, String cpf) {
-        this.nome = nome;
+    //Método que contrói um novo objeto do tipo Pessoa com os seguintes parâmetros
+    public Pessoa(String nomeCompleto, int idade, String cpf, String genero, String profissao, String data_de_chegada) {
+        setNome (nomeCompleto);
         this.idade = idade;
-        this.cpf = cpf;
+        setCpf(cpf);
+        this.genero = genero;
+        this.profissao = profissao;
+        this.data_de_chegada = data_de_chegada;
     }
 
-    public String getNome() {
-        return nome;
+    //Método que retorna o nome completo
+    public String getNomeCompleto() {
+        return primeiroNome + " " + segundoNome + " " + terceiroNome;
     }
+
+    //Setters dos nomes
+    public String getPrimeiroNome(){
+        return primeiroNome;
+    }
+    public String getSegundoNome(){
+        return segundoNome;
+    }
+    public String getTerceiroNome(){
+        return terceiroNome;
+    }
+
+    //Método que trata e seta o cpf 
+    public void setCpf(String cpf) {
+        // Remove caracteres não numéricos (incluindo letras, espaços, etc.)
+        cpf = cpf.replaceAll("[^0-9]", "");
+        
+        // Verificação se o CPF tem 11 dígitos para setar no objeto
+        if (cpf.length() == 11) {
+            this.cpf = cpf;
+        } else {
+            System.out.println("Erro: CPF deve ter exatamente 11 dígitos.");
+        }
+    }
+    
+    public String getCpf() {
+        return cpf;
+    }
+
+    //Método para tratar o nome
+    public void setNome(String nomeCompleto) {
+        // Separa o nome em 3 partes
+        String[] partes = nomeCompleto.trim().split(" ");  // Divide por espaços em branco
+
+        // checa se existem 3 partes 
+        if (partes.length >= 3) {
+            this.primeiroNome = partes[0];
+            this.segundoNome = partes[1];
+            this.terceiroNome = partes[2];
+        } else {
+            System.out.println("Erro: O nome deve conter pelo menos três partes.");
+        }
+    }
+    @Override
+    public String toString() {
+        return "Pessoa [Nome: " + getNomeCompleto() + ", Idade: " + idade + ", CPF: " + cpf + ", Gênero: " + genero + ", Profissão: " + profissao + ", Data de Chegada: " + data_de_chegada + "]";
+    }
+    
 }
--- src/br/ufrn/imd/Request.java
+++ src/br/ufrn/imd/Request.java
@@ -1,17 +1,59 @@
 package br.ufrn.imd;
 
-//Aqui vamos adiciona a pessoa e a musica que ela pediu.
+// Classe para representar um pedido de música ou algo similar
 public class Request {
-    private Pessoa pessoa;
-    private String titulo;
-    private String estilo;
-    private String tempo;
+    private Pessoa pessoa;     // Associação com a classe Pessoa
+    private String titulo;     // Título da música ou item solicitado
+    private String estilo;     // Estilo ou gênero da música/item
+    private String duracao;    // Duração do pedido em formato de tempo (ex.: "03:45")
 
-    public Request(Pessoa pessoa, String titulo, String estilo, String tempo) {
+    // Construtor da classe Request
+    public Request(Pessoa pessoa, String titulo, String estilo, String duracao) {
         this.pessoa = pessoa;
         this.titulo = titulo;
         this.estilo = estilo;
-        this.tempo = tempo;
+        this.duracao = duracao;
     }
 
+    // Getters e Setters
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    // Método toString para facilitar a exibição de informações
+    @Override
+    public String toString() {
+        return "Request [Pessoa: " + pessoa.getNomeCompleto() + 
+               ", Título: " + titulo + 
+               ", Estilo: " + estilo + 
+               ", Duração: " + duracao + "]";
+    }
 }
